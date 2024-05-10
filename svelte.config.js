8f28f38e59330dce6a894440909a80ebc39246a8
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-05-10 11:24:40 +0300

    /

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,9 +14,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/SNA_project',
-		  }
+		// paths: {
+		// 	base: dev ? '' : '/SNA_project',
+		//   }
 	}
 }
 
